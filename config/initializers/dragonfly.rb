172722288b96eee2c3a1f3eae62ad6ac14a23908
kristionas <kristionas@yandex.ru>
2015-03-09 07:06:49 +0700

    fix dragonfly and remove manifest

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -2,13 +2,13 @@ require 'dragonfly'
 
 ## initialize Dragonfly ##
 
-app = Dragonfly[:images]
+app = Dragonfly.app(:images)
 app.configure_with(:rails)
 app.configure_with(:imagemagick)
 
 ## configure it ##
 
-Dragonfly[:images].configure do |c|
+Dragonfly.app(:images).configure do |c|
   # Convert absolute location needs to be specified
   # to avoid issues with Phusion Passenger not using $PATH
   c.convert_command  = `which convert`.strip.presence || "/usr/local/bin/convert"
